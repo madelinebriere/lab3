ebfe9465fe709c801b12db5e09097b78844c8e5f
Maddie Briere <briere.maddie@gmail.com>
2019-04-07 13:41:49 -0400

    Final edits

--- warmup.m
+++ warmup.m
@@ -7,16 +7,27 @@ figure(1)
 hold on
 subplot(2,1,1);
 stem(x)
-title('Warmup a')
+title('Original Signal - Warmup a')
 xlabel('Time (sample)')
 
 subplot(2,1,2);
 stem(result);
-xlabel('Sample')
+title('FFT of Signal - Warmup a')
+xlabel('Sample (value of k)')
 
 Y=(randn(1,256)+1i*randn(1,256))/sqrt(2);
 Z=Y+conj([Y(1) fliplr(Y(2:end))]);
 Z_x=-128:127;
+z = ifft(Z)
+ 
 figure(2)
+hold on
+subplot(2,1,1);
 stem(Z_x, Z)
-title('Warmup b')
+title('FFT of Signal - Warmup b')
+xlabel('Sample (value of k)')
+
+subplot(2,1,2);
+stem(z);
+title('IFFT of FFT - Warmup b')
+xlabel('Time (sample)')
